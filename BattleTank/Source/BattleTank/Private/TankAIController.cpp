4f517a905030a61d9321d135c129efcd5838c110
AjayBS <bsajay.22@gmail.com>
2017-06-26 14:04:14 -0600

    BT06 Added player and AI controller

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller can't find player"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AI controller found player:%s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{	
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
