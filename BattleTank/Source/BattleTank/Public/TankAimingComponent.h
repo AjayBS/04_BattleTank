8b046d8889d4fbd898743f41b2f6a92cbf875b00
AjayBS <bsajay.22@gmail.com>
2017-07-08 19:54:24 -0600

    BT09 Added tank barrel class

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -5,7 +5,10 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Forward declaration
+class UTankBarrel; 
 
+// Holds barrel's properties and elevate method
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankAimingComponent : public UActorComponent
 {
@@ -15,16 +18,12 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-	// Called when the game starts
-	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
-
 	void AimAt(FVector HitLocation, float LaunchSpeed);
-	void SetBarrelReference(UStaticMeshComponent *BarrelToSet);
+	
+	void SetBarrelReference(UTankBarrel *BarrelToSet);
+	
 
 private:
-	UStaticMeshComponent *Barrel = nullptr;
-	
+	UTankBarrel *Barrel = nullptr;
+	void MoveBarrelTowards(FVector AimDirection);
 };
